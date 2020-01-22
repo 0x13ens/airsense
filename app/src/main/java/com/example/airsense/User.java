77d88076f2c3ba75c033b23a301dc2333bff19a2
Ben <core@bens-mbp.home>
2020-01-22 20:34:47 +0000

    setup

--- app/src/main/java/com/example/airsense/User.java
+++ app/src/main/java/com/example/airsense/User.java
@@ -1,7 +1,6 @@
 package com.example.airsense;
 
 //this is very simple class and it only contains the user attributes, a constructor and the getters
-// you can easily do this by right click -> generate -> constructor and getters
 public class User {
 
     private int id;
